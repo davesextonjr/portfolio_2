41ead77cd036e4f7bee42abb35eac6719c60c4cd
David Sexton Jr <davesextonjr@gmail.com>
2024-02-22 15:12:56 -0500

    fix form

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const App = () => {
       <Contact />
       <footer className="p-3 text-center">
         <h6 className="mb-3">DAVE SEXTON</h6>
-        <p>Much thanks to John Alex at <a href="https://www.youtube.com/@CodeAProgram" target="_blank">codeaprogram</a></p>
+        <p>Much thanks to John Alex at <a href="https://www.youtube.com/@CodeAProgram" target="_blank">codeaprogram</a> for the starter code</p>
       </footer>
     </div>
   );
